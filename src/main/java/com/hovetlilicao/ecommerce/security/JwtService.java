1f3e1b33860db728973273b192522b37e3ed4bbf
theKormann <kormannmatheus@gmail.com>
2025-05-07 21:48:25 -0300

    adiciona o JWS Service

--- src/main/java/com/hovetlilicao/ecommerce/security/JwtService.java
+++ src/main/java/com/hovetlilicao/ecommerce/security/JwtService.java
@@ -1,9 +1,33 @@
 package com.hovetlilicao.ecommerce.security;
 
-import lombok.RequiredArgsConstructor;
+
+import com.hovetlilicao.ecommerce.models.User;
+import lombok.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
 @Service
-@RequiredArgsConstructor
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(Date.from(Instant.now().plus(1, ChronoUnit.DAYS)))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser().setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
 }
