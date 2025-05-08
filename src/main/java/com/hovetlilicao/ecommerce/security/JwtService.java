d246148f14cc4511b0777914a435ef5e480dd514
theKormann <kormannmatheus@gmail.com>
2025-05-08 16:37:17 -0300

    adiciona o jwt securiy

--- src/main/java/com/hovetlilicao/ecommerce/security/JwtService.java
+++ src/main/java/com/hovetlilicao/ecommerce/security/JwtService.java
@@ -14,20 +14,4 @@ public class JwtService {
 
     @Value("${jwt.secret}")
     private String secretKey;
-
-    public String generateToken(User user) {
-        return Jwts.builder()
-                .setSubject(user.getUsername())
-                .setIssuedAt(new Date())
-                .setExpiration(Date.from(Instant.now().plus(1, ChronoUnit.DAYS)))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
-    }
-
-    public String extractUsername(String token) {
-        return Jwts.parser().setSigningKey(secretKey)
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
 }
